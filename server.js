04456972a135c8c926544e3640aa28e0c5966d6d
Ashukla011 <avnishkumar492862@gmail.com>
2025-10-29 15:14:05 +0530

    ai

--- server.js
+++ server.js
@@ -16,10 +16,24 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://hirely-9zh57sniu-kavanish011gmailcoms-projects.vercel.app",
+];
+
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 
 app.use(cors(corsOptions));
 
